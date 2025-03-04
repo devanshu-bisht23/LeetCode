43e92f465826c76de3a3a2418699cd447f2f45d4
devanshu-bisht23 <devanshubisht23@gmail.com>
2025-03-05 01:47:54 +0700

    Time: 0 ms (100%), Space: 9.3 MB (9.75%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        vector<string> res;
+        int n = nums.size();
+        if (n == 0) return res; 
+
+        int start = 0; // keeps address track
+
+        for (int i = 1; i <= n; i++) { // notice <= n so we check after the last element
+
+            if (i == n || nums[i] != nums[i-1] + 1) { // if prev value is less than 1 of the current value then its a seq
+                if (start == i - 1) { // eg: 0 == 0 or 2==2 etc...
+                    // for single element range
+                    res.push_back(to_string(nums[start]));
+                } else {
+                    // for multiple element range
+                    res.push_back(to_string(nums[start]) + "->" + to_string(nums[i-1]));
+                }
+                start = i; //update start
+            }
+            
+        }
+
+        return res;
+    }
+};
