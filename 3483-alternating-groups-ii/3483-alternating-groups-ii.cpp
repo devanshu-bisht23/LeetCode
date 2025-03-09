6cc4e74f082b48a81ae792306e1d55d8743eb8e6
Devanshu Bisht <devanshubisht23@gmail.com>
2025-03-09 18:34:03 +0530

    Time: 67 ms (74.48%), Space: 102.5 MB (91.84%) - LeetHub

---
+++ 3483-alternating-groups-ii/3483-alternating-groups-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int numberOfAlternatingGroups(vector<int>& colors, int k) {
+
+        int n = colors.size(); //length of colors array
+        int l = 0; // first pointer (left)
+        int output = 0; // what we will return at the end
+        for(int r=1; r< n + k - 1; r++){
+
+            if(colors[r%n]==colors[(r-1)%n]){ //it means the colors are not alternating
+                l=r; // we slide the window
+            }
+
+            if(r-l+1 > k){ //if window becomes bigger than k
+                l++;
+            }
+
+            if(r - l + 1 == k){ // if window is equals to k
+                output++;
+            }
+
+        }
+    return output;
+    }
+};
