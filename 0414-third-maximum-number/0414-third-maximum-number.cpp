defcb6570ef26a29d34d631b8768854a2fefb131
Devanshu Bisht <devanshubisht23@gmail.com>
2025-03-12 01:29:10 +0530

    Time: 127 ms (5.71%), Space: 12.8 MB (79.48%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        int n = nums.size();
+
+        // Selection sort but descending order
+        for(int i = 0; i < n; i++) {
+            int max_idx = i;
+            for(int j = i + 1; j < n; j++) {
+                if(nums[j] > nums[max_idx]) {
+                    max_idx = j;
+                }
+            }
+            swap(nums[i], nums[max_idx]);
+        }
+
+        int uniqueCount = 1;
+        for(int i = 1; i < n; i++) {
+            if(nums[i] != nums[i - 1]) { // Only for counting unique values
+                uniqueCount++;
+                if(uniqueCount == 3) {
+                    return nums[i]; 
+                }
+            }
+        }
+
+        return nums[0]; // return max if no 3 distinct elements in arr
+    }
+};
