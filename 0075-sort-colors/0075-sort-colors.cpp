a0679a763c29aaab49c6ef3bc3c63599b40fb3e3
Devanshu Bisht <devanshubisht23@gmail.com>
2025-03-14 01:27:21 +0530

    Time: 0 ms (100%), Space: 11.6 MB (70.87%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,43 +1,24 @@
 class Solution {
 public:
-
-    int partition (vector<int>&nums, int l, int r){
-
-        int x = l + rand() % (r - l + 1);
-        swap(nums[x], nums[r]);  
-
-        int pivot = nums[r];
-        int i = l-1;
-
-        for(int j = l; j<r; j++){
-            if(nums[j]<pivot){
-                i++;
-                swap(nums[i],nums[j]);
-            }
-        }
-
-        swap(nums[i+1],nums[r]);
-        return (i+1);
-
-    }
-
-    // vector<int> 
-    void quickSort(vector<int>&nums, int l, int r){
-        if(l<r){
-            int p = partition(nums, l, r);
-            quickSort(nums, l, p-1);
-            quickSort(nums, p+1, r);
-        }
-
-        // return nums;
-    }
-
     void sortColors(vector<int>& nums) {
         
-        // srand(time(0));
-        // vector<int> arr =
-         quickSort(nums,0, nums.size()-1);
-        // return arr; 
-        
+        unordered_map<int,int> hashMap;
+
+        for(int num: nums){
+            hashMap[num]++;
+        }
+
+        int j =0;
+        for(int i = 0; i<3;i++){
+
+            while(hashMap[i]>0){
+                nums[j++] = i; 
+                hashMap[i]--;
+            }
+      
+
+        }
+
+
     }
-}; 
+};
