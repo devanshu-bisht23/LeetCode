a2dca706cd837688bc3ed8870216de29cf0d2102
Devanshu Bisht <devanshubisht23@gmail.com>
2025-03-14 00:33:44 +0530

    Time: 0 ms (100%), Space: 11.6 MB (70.52%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,8 +1,39 @@
 class Solution {
 public:
+
+    int partition (vector<int>&nums, int l, int r){
+
+        int pivot = nums[r];
+        int i = l-1;
+
+        for(int j = l; j<r; j++){
+            if(nums[j]<pivot){
+                i++;
+                swap(nums[i],nums[j]);
+            }
+        }
+
+        swap(nums[i+1],nums[r]);
+        return (i+1);
+
+    }
+
+    // vector<int> 
+    void quickSort(vector<int>&nums, int l, int r){
+        if(l<r){
+            int p = partition(nums, l, r);
+            quickSort(nums, l, p-1);
+            quickSort(nums, p+1, r);
+        }
+
+        // return nums;
+    }
+
     void sortColors(vector<int>& nums) {
         
-        sort(begin(nums),end(nums));
+        // vector<int> arr =
+         quickSort(nums,0, nums.size()-1);
+        // return arr; 
         
     }
-};
+}; 
