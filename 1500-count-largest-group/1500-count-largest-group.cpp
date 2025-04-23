a1e4281f827dcebabad7f41df3dfc69ab84dcca4
Devanshu Bisht <devanshubisht23@gmail.com>
2025-04-23 23:55:58 +0530

    Time: 3 ms (33.9%), Space: 8.2 MB (44.07%) - LeetHub

---
+++ 1500-count-largest-group/1500-count-largest-group.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int countLargestGroup(int n) {
+
+        unordered_map<int,int> hashMap;
+        int count = 0;
+        
+
+        for(int i=1; i<=n; i++){
+            int digSum = dig(i); // we get digit sum here
+            hashMap[digSum]++;
+        }
+
+        int x;
+        
+        for(auto &i: hashMap){
+            if(i.second > x){
+            x = i.second;
+            }
+        }
+
+        for(auto &i: hashMap){
+            if(i.second == x){
+                count++;
+            }
+        }
+
+    return count;
+
+    }
+
+    int dig(int n){
+
+        int sum = 0;
+
+        while(n>0){
+            sum += n % 10;
+            n = n/10;
+        }
+
+        return sum;
+
+    }
+};
