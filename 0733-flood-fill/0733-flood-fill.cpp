7fe1c02df718540bd1f1e16d89f9cbad81e362ed
Devanshu Bisht <devanshubisht23@gmail.com>
2025-07-14 18:48:11 +0700

    Time: 0 ms (100%), Space: 18.3 MB (36.55%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,42 +1,38 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        
-        
+
         int n = image.size();
         int m = image[0].size();
 
-        int originalColor = image[sr][sc];
-
-        if (originalColor == color) {
-            return image;
+        int src = image[sr][sc];
+        if (src == color) {
+            return image; 
         } 
 
-        queue<pair<int,int>> q; 
-
+        queue<pair<int,int>> q;
         q.push({sr,sc});
-        
         image[sr][sc] = color;
         
 
-        while(!q.empty()){
-            int u = q.front().first;
-            int v = q.front().second;
+        int drow[] = {-1,0,1,0}; 
+        int dcol[] = {0,1,0,-1};
 
+        while(!q.empty()){
+
+            int r = q.front().first;
+            int c = q.front().second;
             q.pop();
 
-            int drow[] = {-1,0,1,0};
-            int dcol[] = {0,1,0,-1};
-
             for(int i=0; i<4; i++){
-                int nrow = u + drow[i];
-                int ncol = v + dcol[i];
 
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && image[nrow][ncol] == originalColor){ 
+                int nrow = r + drow[i];
+                int ncol = c + dcol[i];
+
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && image[nrow][ncol] == src){
                     image[nrow][ncol] = color;
                     q.push({nrow,ncol});
                 }
-                
             }
         }
 
