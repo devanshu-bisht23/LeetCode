5d85a8ce94587edef67eca06c8361fc91de71bfc
Devanshu Bisht <devanshubisht23@gmail.com>
2025-07-17 18:25:14 +0700

    Time: 65 ms (10.72%), Space: 106 MB (29.38%) - LeetHub

--- 1966-frequency-of-the-most-frequent-element/1966-frequency-of-the-most-frequent-element.cpp
+++ 1966-frequency-of-the-most-frequent-element/1966-frequency-of-the-most-frequent-element.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    int canMake(vector<int>& nums, int k, int freq, int n) {
+    int isValid(vector<int>& nums, int k, int mid, int n) {
 
         long long windowSum = 0;
 
-        for (int i=0; i < freq-1; i++) {
+        for (int i=0; i<mid-1; i++) {
             windowSum += nums[i];
-        }
+        } 
 
-        for (int i = freq-1; i<n; i++) {
+        for (int i = mid-1; i<n; i++) {
             windowSum += nums[i];
-            long long target = 1LL * nums[i] * freq;
-            if (target - windowSum <= k)
+            long long totalMax = 1LL * nums[i] * mid; 
+            if (totalMax - windowSum <= k){ 
                 return 1;
-            windowSum -= nums[i - freq + 1];
+            }
+            windowSum -= nums[i - mid + 1]; 
         }
 
         return 0;
@@ -25,13 +26,15 @@ public:
         int l=1, r=n, res=1;
 
         while (l <= r) {
+
             int mid = l + (r - l) / 2;
-            if (canMake(nums,k, mid, n)) {
+            if (isValid(nums,k, mid, n)) {
                 res = mid;
                 l = mid + 1;
             } else {
                 r = mid - 1;
             }
+            
         }
 
         return res;
