9a44ef6323a66e01742f63edea6be33cd44df6b0
Devanshu Bisht <devanshubisht23@gmail.com>
2025-07-23 00:18:52 +0700

    Time: 0 ms (100%), Space: 11.1 MB (75.63%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
@@ -1,20 +1,35 @@
+// Given an array nums
+// return true if the array was originally sorted, then rotated some number of positions
+// Otherwise, return false.
+
+// There may be duplicates in the original array.
+
+// An array A rotated by x positions results in an array B
+// B[i] == A[(i+x) % A.length] for every valid index i
+
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        int n = nums.size();
+
         int count = 1;
-        if(n==1) return true;
-        for(int i =0; i<2*n; i++){
-            if(nums[i%n] <= nums[(i+1) % n]){
-                count +=1;
+        int n = nums.size();
+
+        if(n==1){
+            return true;
+        }
+        
+        for(int i=1; i<n*2; i++){
+
+            if(nums[(i-1) % n] <= nums[i % n]){
+                count++;
+                if(count == n){
+                    return true;
+                }
             }else{
                 count = 1;
             }
 
-            if(count == n){
-                return true;
-            }
         }
-       return false;
+    return false;
     }
 };
