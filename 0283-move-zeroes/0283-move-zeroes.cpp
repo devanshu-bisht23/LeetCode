1724a3efebc04cb5ad81d2bde9e89defd21dbfae
Devanshu Bisht <devanshubisht23@gmail.com>
2025-07-25 01:42:55 +0700

    Time: 0 ms (100%), Space: 23.8 MB (80.54%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        
+        int n = nums.size();
+        int j=-1;
+        for(int i=0; i<n; i++){
+            if(nums[i]==0){
+                j=i;
+                break;
+            }
+        }
+        
+        if (j == -1) return;
+
+        int i=j+1; 
+        while(i<n){
+            if(nums[i]!=0){ 
+                int temp = nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                j++;
+            }
+            i++;
+        }
+
+    }
+};
