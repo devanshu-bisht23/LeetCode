75b09f3989583682aeac1c8fbaa5acba30df2b4b
Devanshu Bisht <devanshubisht23@gmail.com>
2025-08-23 19:31:59 +0700

    Time: 0 ms (100%), Space: 17.6 MB (89.37%) - LeetHub

--- 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
 
-    void insertOdd(string &ans, string num, int n){
+    // void insertOdd(string &ans, string num, int n){
 
-        for(int i=0; i<=n; i++){
-            ans+= num[i];
-        }
+    //     for(int i=0; i<=n; i++){
+    //         ans+= num[i];
+    //     }
 
-    }
+    // }
 
     string largestOddNumber(string num) {
         
         int n = num.size();
-        string ans = "";
+        // string ans = "";
         for(int i=n-1; i>=0; i--){
 
             char c = num[i];
             int nums = c - '0';
 
             if(nums%2 != 0){
-                insertOdd(ans,num,i); 
-                break;
+                
+                return num.substr(0,i+1);
+                
+                // insertOdd(ans,num,i); 
+                // break;
             }
 
         }
-    return ans;
+    // return ans;
+    return "";
     }
 };
