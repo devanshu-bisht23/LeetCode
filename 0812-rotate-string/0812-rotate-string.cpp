d5b7b44b23be81471e2ca964e25df4a207d37e96
Devanshu Bisht <devanshubisht23@gmail.com>
2025-08-24 16:35:48 +0700

    Time: 0 ms (100%), Space: 8.5 MB (17.92%) - LeetHub

---
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        
+        if(s.size() != goal.size()) return false;
+
+        string str = s+s;
+
+        for(int i = 0; i < s.size(); i++) {
+            if(str.substr(i, s.size()) == goal) {
+                return true;
+            }
+        }
+    return false;
+    }
+};
