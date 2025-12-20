14214c7d94bc132c321574eeb043e0900bb2790e
Devanshu Bisht <devanshubisht23@gmail.com>
2025-12-20 12:38:30 +0700

    Time: 1 ms (5.79%), Space: 8.7 MB (16.5%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+
+    
+    int steps(int n, vector<int> &dp){
+
+        if(n==0 || n==1){
+            return 1;
+        }
+
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+
+        return dp[n] = steps(n-1,dp) + steps(n-2,dp); 
+
+    }
+
+
+    int climbStairs(int n) {
+
+        vector<int> dp(n+1,-1);
+        return steps(n, dp);
+
+    }
+};
