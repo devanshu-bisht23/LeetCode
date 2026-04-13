ba396f6c30fd415b4f11f5b95bfd7d7a185a22f9
Devanshu Bisht <devanshubisht23@gmail.com>
2026-04-14 01:20:01 +0530

    Time: 4 ms (50.47%), Space: 9.8 MB (81.04%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -29,3 +29,37 @@ public:
         return curr;
     }
 };
+
+
+// class Solution {
+// public:
+//     string countAndSay(int n) {
+
+//         string finalres = "";
+
+//         if(n == 1) return "1";
+
+//         for(int i=2; i<=n; i++){
+
+//             int counter = 1; int tempIdx = 0;
+//             string res = "";
+//             string str = "11";
+
+//             for(int j=1; j<res.length(); j++){
+//                 if(str[j-1] == str[j]){
+//                     counter++;
+//                 }else{
+//                     res[tempIdx] = counter;
+//                     res[tempIdx+1] = str[tempIdx];
+//                     tempIdx += 2;
+//                     counter = 1;
+//                 }
+                
+//             finalres = res;
+
+//         }
+        
+//     return finalres;
+
+//     }
+// };
