b57302149e248b38f7d2dbeb6f887fb98b835279
Devanshu Bisht <devanshubisht23@gmail.com>
2026-07-06 01:06:45 +0530

    Time: 118 ms (37.15%), Space: 31.1 MB (36.89%) - LeetHub

---
+++ 0087-scramble-string/0087-scramble-string.cpp
@@ -0,0 +1,37 @@
+class Solution {
+    unordered_map<string, bool> mem;
+
+    bool solve(string s1, string s2) {
+        if (s1.size() == 1)
+            return s1 == s2;
+
+        if (s1 == s2)
+            return true;
+
+        string key = s1 + s2;
+
+        if (mem.find(key) != mem.end())
+            return mem[key];
+
+        int n = s1.size();
+
+        for (int i = 1; i < n; i++) {
+            if (
+                (solve(s1.substr(0, i), s2.substr(0, i)) &&
+                 solve(s1.substr(i), s2.substr(i))) ||
+
+                (solve(s1.substr(0, i), s2.substr(n - i)) &&
+                 solve(s1.substr(i), s2.substr(0, n - i)))
+            ) {
+                return mem[key] = true;
+            }
+        }
+
+        return mem[key] = false;
+    }
+
+public:
+    bool isScramble(string s1, string s2) {
+        return solve(s1, s2);
+    }
+};
