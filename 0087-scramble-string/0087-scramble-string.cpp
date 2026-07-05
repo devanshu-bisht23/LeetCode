1dd61009abbf3c4d8a22f5f394ce6f9a4017f4dc
Devanshu Bisht <devanshubisht23@gmail.com>
2026-07-06 01:07:41 +0530

    Time: 115 ms (37.15%), Space: 31.2 MB (36.76%) - LeetHub

--- 0087-scramble-string/0087-scramble-string.cpp
+++ 0087-scramble-string/0087-scramble-string.cpp
@@ -17,11 +17,11 @@ class Solution {
 
         for (int i = 1; i < n; i++) {
             if (
-                (solve(s1.substr(0, i), s2.substr(0, i)) &&
-                 solve(s1.substr(i), s2.substr(i))) ||
+                (solve(s1.substr(0, i), s2.substr(0, i)) && solve(s1.substr(i), s2.substr(i))) 
+                 
+                ||
 
-                (solve(s1.substr(0, i), s2.substr(n - i)) &&
-                 solve(s1.substr(i), s2.substr(0, n - i)))
+                (solve(s1.substr(0, i), s2.substr(n - i)) && solve(s1.substr(i), s2.substr(0, n - i)))
             ) {
                 return mem[key] = true;
             }
