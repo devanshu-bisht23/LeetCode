45565704c96470698eb26eddd9f5ca66e70a6a04
Devanshu Bisht <devanshubisht23@gmail.com>
2026-07-26 16:20:52 +0530

    Time: 4 ms (5.52%), Space: 18.5 MB (14.38%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -5,35 +5,38 @@ public:
         int n = image.size();
         int m = image[0].size();
 
-        int src = image[sr][sc];
-        if (src == color) {
-            return image; 
-        } 
+        vector<vector<int>> visited(n, vector(m,0));
 
         queue<pair<int,int>> q;
-        q.push({sr,sc});
-        image[sr][sc] = color;
-        
 
-        int drow[] = {-1,0,1,0}; 
+        q.push({sr,sc});
+        int drow[] = {-1,0,1,0};
         int dcol[] = {0,1,0,-1};
+        int initialColor = image[sr][sc];
+        
+        visited[sr][sc] = 1;
+        image[sr][sc] = color;
 
         while(!q.empty()){
-
+            
             int r = q.front().first;
             int c = q.front().second;
             q.pop();
 
             for(int i=0; i<4; i++){
 
-                int nrow = r + drow[i];
-                int ncol = c + dcol[i];
+                int nrow = r+drow[i];
+                int ncol = c+dcol[i];
 
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && image[nrow][ncol] == src){
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && image[nrow][ncol] == initialColor && visited[nrow][ncol] == 0){
+                    visited[nrow][ncol] = 1;
                     image[nrow][ncol] = color;
                     q.push({nrow,ncol});
                 }
+
             }
+
+
         }
 
         return image;
